87e2e85a991dd19dbed12d432b0d21ce4b47ee54
NR <95234863+sraww@users.noreply.github.com>
2025-10-20 17:20:01 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ namespace SpeechRecognition
     static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
